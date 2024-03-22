0860e2b75d49083f504bbb87e9f9940cf9328eeb
Jan Bannister <jan.bannister@gmail.com>
2024-03-22 10:04:26 +0000

    Restaurant list

--- Pint/Pint/View/Restaurant/RestaurantListView.swift
+++ Pint/Pint/View/Restaurant/RestaurantListView.swift
@@ -6,12 +6,51 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct RestaurantListView: View {
     @State private var restaurants = Restaurant.example
     var body: some View {
-        ForEach(restaurants) { restaurant in
-            RestaurantView(restaurant: restaurant)
+        NavigationView {
+            
+            Form {
+                ForEach(restaurants) { restaurant in
+                    NavigationLink(destination: RestaurantView(restaurant: restaurant)) {
+                        Text(restaurant.name)
+                    }
+                }
+                .onDelete(perform: deleteRestaurants)
+            }
+            .navigationBarTitle("Restaurant List", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: addRestaurants) {
+                        Label("Add Person", systemImage: "plus")
+                    }
+                }
+            }
+
+        }
+    }
+    
+    private func addRestaurants() {
+        withAnimation {
+            let newRestaurant = Restaurant(name: "Anon",
+                                           address: "Address",
+                                           coordinate: CLLocationCoordinate2D(latitude: 51.453630,
+                                                                              longitude: -0.290940))
+            restaurants.append(newRestaurant)
+        }
+    }
+
+    private func deleteRestaurants(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                // restaurants[index] // Delete
+            }
         }
     }
 }
