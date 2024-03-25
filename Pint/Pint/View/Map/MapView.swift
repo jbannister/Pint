fe237bc90f4f5a862dacd4e585e367d12ec5e8d8
Jan Bannister <jan.bannister@gmail.com>
2024-03-25 19:13:23 +0000

    cameraPosition + regions + search text

--- Pint/Pint/View/Map/MapView.swift
+++ Pint/Pint/View/Map/MapView.swift
@@ -9,46 +9,110 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @State private var cameraPosition: MapCameraPosition = .region(MKCoordinateRegion())
+    //@State private var mapSelection: MKMapItem?
+    @State private var route: MKRoute?
+    @State private var routeDestination: MKMapItem?
+    @State private var searchText = ""
+    @State private var results = [MKMapItem]()
     
     var persons = [
         Person(name: "Jan",
-                 address: "49 King George, Richmond",
-                 coordinate:
-                    CLLocationCoordinate2D(latitude: 51.453630,
-                                           longitude: -0.290940)),
+               address: "49 King George, Richmond",
+               coordinate:
+                CLLocationCoordinate2D(latitude: 51.453630,
+                                       longitude: -0.290940)),
         Person(name: "MC",
-                 address: "Church Road, Richmond",
-                 coordinate: CLLocationCoordinate2D(latitude: 51.460864,
-                                                    longitude: -0.297149)),
+               address: "Church Road, Richmond",
+               coordinate: CLLocationCoordinate2D(latitude: 51.460864,
+                                                  longitude: -0.297149)),
         
         Person(name: "Shane Donnelly",
-                 address: "New Road, Reading",
-                 coordinate: CLLocationCoordinate2D(latitude: 51.443950,
-                                                    longitude: -0.952680)),
+               address: "New Road, Reading",
+               coordinate: CLLocationCoordinate2D(latitude: 51.443950,
+                                                  longitude: -0.952680)),
         
         Person(name: "Colin O'Sullivan",
-                 address: "West Norwood, London",
-                 coordinate: CLLocationCoordinate2D(latitude: 51.43462818592707,
-                                                    longitude: -0.10384535281600316)),
+               address: "West Norwood, London",
+               coordinate: CLLocationCoordinate2D(latitude: 51.43462818592707,
+                                                  longitude: -0.10384535281600316)),
         
         Person(name: "Tall Connaughton",
-                 address: "Folgate Street, London",
-                 coordinate: CLLocationCoordinate2D(latitude: 51.52080696041292,
-                                                    longitude: -0.07618033285277254))
+               address: "Folgate Street, London",
+               coordinate: CLLocationCoordinate2D(latitude: 51.52080696041292,
+                                                  longitude: -0.07618033285277254))
         
         
     ]
-        
+    
     var body: some View {
-        Map {
+                    
+        Map(position: $cameraPosition) {
             ForEach(persons) { person in
                 Marker(person.name, coordinate: person.coordinate)
             }
-        }
             
+            ForEach(results, id: \.self) { result in
+                Marker(result.name ?? "Anon", coordinate: result.placemark.coordinate)
+                    .tint(.blue)
+            }
+        }
+        .overlay(alignment: .top) {
+            TextField("Search for a location...", text: $searchText)
+                .font(.subheadline)
+                .padding(12)
+                .background(.white)
+                .padding()
+                .shadow(radius:10)
+        }
+        .onSubmit(of: .text) {
+            // on return? 
+            Task { await searchPlaces() }
+        }
+        .mapControls{
+            MapCompass()
+            MapPitchToggle()
+            MapUserLocationButton()
+        }
+        .task {
+            fetchRoute()
+        }
     }
+    
+    var routePolyline: MKPolyline? {
+        route?.polyline
+    }
+    
+    func searchPlaces() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.region = persons[0].region
+
+        let results = try? await MKLocalSearch(request: request).start()
+        self.results = results?.mapItems ?? []
+    }
+    
+    func fetchRoute() {
+        let request = MKDirections.Request()
+        request.source = persons[0].mapItem
+        request.destination = persons[1].mapItem
+        request.transportType = .automobile
+        
+        Task {
+            let result = try? await MKDirections(request: request).calculate()
+            route = result?.routes.first
+            withAnimation(.easeInOut) {
+                if let rect = route?.polyline.boundingMapRect {
+                    cameraPosition = .rect(rect)
+                }
+            }
+        }
+    }
+    
 }
 
 #Preview {
     MapView()
 }
+
+
